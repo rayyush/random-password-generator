4da339e3cc5d4d2256e1a4b740742ac914cef4d0
rayyush <142962272+rayyush@users.noreply.github.com>
2025-04-30 07:59:15 +0000

    final code ready

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import random
 
 password = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz!@#$%^&*()"
 length_password = int(input("Enter the length of the password required: "))
+a = "".join(random.sample(password, length_password))
+
+print(f"Your password is {a}")
